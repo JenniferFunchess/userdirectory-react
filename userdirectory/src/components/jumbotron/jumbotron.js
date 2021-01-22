19877618b1c13103e7e46cd2bf6550238e37817f
Jennifer Funchess <jfunchess@ymail.com>
2021-01-21 21:51:28 -0600

    updated searchbar component

--- userdirectory/src/components/jumbotron/jumbotron.js
+++ userdirectory/src/components/jumbotron/jumbotron.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "../jumbotron/jumbotron.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function Jumbotron() {
   return (
---
+++ userdirectory/src/components/search/search.js
@@ -0,0 +1,23 @@
+import React from "react";
+import "../search/search.css";
+
+const Search = (props) => {
+  return (
+    <form>
+      <div className="form-group search-widget">
+        <div className="input-group mb-3">
+          <input
+            onChange={props.handleInputChange}
+            value={props.search}
+            name="search"
+            type="text"
+            className="form-control"
+            placeholder="Search directory for Employee"
+            id="search"
+          />
+        </div>
+      </div>
+    </form>
+  );
+};
+export default Search;
